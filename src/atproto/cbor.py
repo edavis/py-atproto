9a05e4fd1c7b656f870faa1971bdc141fe35fb7f
Eric Davis <eric@davislv.com>
2023-09-30 15:18:36 -0700

    Handle byte strings, refactor tests

--- src/atproto/cbor.py
+++ src/atproto/cbor.py
@@ -3,6 +3,7 @@ from enum import Enum
 class MajorType(Enum):
     UNSIGNED_INT = 0
     NEGATIVE_INT = 1
+    BYTE_STRING = 2
 
 def decode_head(stream):
     (stream_head,) = stream.read(1)
@@ -12,7 +13,7 @@ def decode_head(stream):
     if additional_info < 24:
         if major_type == MajorType.NEGATIVE_INT:
             return major_type, -1 - additional_info
-        elif major_type == MajorType.UNSIGNED_INT:
+        else:
             return major_type, additional_info
 
     byte_lengths = {24: 1, 25: 2, 26: 4, 27: 8}
@@ -21,3 +22,15 @@ def decode_head(stream):
         if major_type == MajorType.NEGATIVE_INT:
             return major_type, -1 - int.from_bytes(byte_value, 'big')
         return major_type, int.from_bytes(byte_value, 'big')
+
+def decode_body(stream):
+    major_type, info = decode_head(stream)
+
+    if major_type in {MajorType.UNSIGNED_INT, MajorType.NEGATIVE_INT}:
+        return info
+
+    elif major_type == MajorType.BYTE_STRING:
+        value = stream.read(info)
+        if len(value) != info:
+            raise EOFError()
+        return value
